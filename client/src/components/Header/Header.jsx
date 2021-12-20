bf8d9ebdcbff7dbb0f058682dc02dd8979bc8f59
talgat.aimbetov.b@gmail.com <84536810+Frataim@users.noreply.github.com>
2021-12-20 19:28:07 +0300

    Release 1_1

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar>
+        <h1>Hello</h1>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
+
